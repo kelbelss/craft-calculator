0652d75aa4f46ab92addfc5233ba1828a7c9915a
kelbelss <124790103+kelbelss@users.noreply.github.com>
2023-12-03 19:10:05 +0200

    Added Product

--- index.js
+++ index.js
@@ -7,20 +7,25 @@ for (i = 0; i < accordion.length; i++) {
     })
 };
 
+
+// CeraCast
 document.getElementById('input').addEventListener('submit', function(event) {
     event.preventDefault(); 
  
-    var water = document.getElementById('input-amount').value; 
-    water = water.replace(/[a-zA-Z]/g, '');
+    var liquid = document.getElementById('input-amount').value; 
+    liquid = liquid.replace(/[a-zA-Z]/g, '');
     
-    if (water == "") {
+    if (liquid == "") {
         alert("Please enter a value.");
         return;
      }
 
-    var powder = (water * 1.4).toFixed(2); 
+    ceraLiquid = liquid * 0.76,89;
+    // useLiquid = (ceraLiquid * 0.76,8);
+    var powder = (ceraLiquid * 1.4).toFixed(1); 
+    // 76.8%
 
-    document.getElementById('powderOutput').textContent = 'You need ' + powder + ' grams of powder for ' + water + ' grams of water.'; 
+    document.getElementById('powderOutput').textContent = 'You need ' + powder + ' grams of powder for ' + ceraLiquid.toFixed(1) + ' grams of liquid.'; 
 });
 
 
@@ -28,4 +33,31 @@ document.getElementById('resetBtn').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('input-amount').value = '';
     document.getElementById('powderOutput').textContent = "";
+}); 
+
+
+// QuickCast
+document.getElementById('input2').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+ 
+    var liquid = document.getElementById('input-amount2').value; 
+    liquid = liquid.replace(/[a-zA-Z]/g, '');
+    
+    if (liquid == "") {
+        alert("Please enter a value.");
+        return;
+     }
+
+    quickLiquid = liquid * 0.59,73
+    var powder = (quickLiquid * 1.8).toFixed(1); 
+    // 59.73%
+
+    document.getElementById('powderOutput2').textContent = 'You need ' + powder + ' grams of powder for ' + quickLiquid.toFixed(1) + ' grams of water.'; 
+});
+
+
+document.getElementById('resetBtn2').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('input-amount2').value = '';
+    document.getElementById('powderOutput2').textContent = "";
 }); 
